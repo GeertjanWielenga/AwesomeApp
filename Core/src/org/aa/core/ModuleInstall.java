3395b3f23914f4f51a88f28f77a632105dde8de0
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-12-10 12:56:43 +0100

    Adding code for removing the border.

--- Core/src/org/aa/core/ModuleInstall.java
+++ Core/src/org/aa/core/ModuleInstall.java
@@ -1,7 +1,9 @@
 package org.aa.core;
 
 import static java.lang.Thread.sleep;
+import javax.swing.BorderFactory;
 import javax.swing.UIManager;
+import org.netbeans.swing.tabcontrol.plaf.DefaultTabbedContainerUI;
 import org.openide.modules.OnStart;
 import org.openide.windows.OnShowing;
 
@@ -16,6 +18,8 @@ public class ModuleInstall implements Runnable {
         } catch (InterruptedException ex) {
         }
         UIManager.put("EditorTabDisplayerUI", "org.aa.core.NoTabsTabDisplayerUI");
+        //Optionally, remove the border too:
+        //UIManager.put(DefaultTabbedContainerUI.KEY_EDITOR_CONTENT_BORDER, BorderFactory.createEmptyBorder());
     }
 
 }
