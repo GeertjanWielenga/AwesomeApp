0831111fd078e07cb01c7dba91c2817362b93088
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-12-10 12:30:43 +0100

    Editor tab removal first checkin.

---
+++ Core/src/org/aa/core/ModuleInstall.java
@@ -0,0 +1,24 @@
+package org.aa.core;
+
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import org.openide.modules.OnStart;
+import org.openide.windows.OnShowing;
+
+@OnShowing
+@OnStart
+public class ModuleInstall implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            sleep(500);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ModuleInstall.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        UIManager.put("EditorTabDisplayerUI", "org.aa.core.NoTabsTabDisplayerUI");
+    }
+
+}
